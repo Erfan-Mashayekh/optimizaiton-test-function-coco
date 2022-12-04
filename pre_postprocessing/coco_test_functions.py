d05e037737ef1a00f387d2d76393ca8c41940c62
Erfan-Mashayekh <mashayekh.e@gmail.com>
2022-12-04 23:52:13 +0100

    add constraints

--- pre_postprocessing/coco_test_functions.py
+++ pre_postprocessing/coco_test_functions.py
@@ -9,7 +9,7 @@ Functions mostly from: https://www.sfu.ca/~ssurjano/index.html
 @author: Lisa Pretsch
 """
 import numpy as np
-from math import exp, pi
+
 
 class TestFunction:
     # Instance attributes
@@ -32,23 +32,25 @@ class COCOTestFunction(TestFunction):
     Usually evaluated on [-5,5]^d
     Global minimum at x_i=1 where f(x)=0
     """
-    def __init__(self, id, constr, dim, xopt, fopt):
+    def __init__(self, id, constr_number, dim, xopt, fopt, ystar):
         super().__init__(
             name='F1',
             dim=dim,
-            lb=-5*np.ones(dim),
-            ub=5*np.ones(dim))
+            lb=-10*np.ones(dim),
+            ub=10*np.ones(dim))
 
         self.id = id
-        self.constr = constr
-        self.xopt = xopt        # location of optimal x
+        self.xopt = xopt        # location of optimal point
         self.fopt = fopt        # optimal value        
+        self.ystar = ystar      # location of constrained optimal point
+        self.constr_number = constr_number
+        self.a = np.zeros((self.constr_number, self.dim))
 
 
     def tosz(self,x):
         """
         Non-Linear Transformation
-        Rn → Rn,  for any positive integer n (n = 1 and n = D are used in the following), maps element-wise
+        Rn → Rn,  for any positive integer n (n = 1 and n = dim are used in the following), maps element-wise
         """
         xhat = np.where(x!=0, np.log(abs(x)), 0)
         c1 = np.where(x>0, 7.9, 3.1)
@@ -59,16 +61,64 @@ class COCOTestFunction(TestFunction):
 
     def evaluate(self, x):
 
-        xopt = self.xopt[0] * np.ones(x.shape)[0::]
+        xopt = self.xopt[0] * np.ones(x.shape)[0]
 
         if self.id == 1:     # F1            
             z = x - xopt
             f = np.sum(z ** 2, axis=0) + self.fopt
             return f    
 
-        elif self.id == 2:   # F2
-            tosz = self.tosz(x)
-            z = tosz * (x - xopt)
-            print(z.shape)
-            f = np.sum()
-            return 
+        elif self.id == 2:   # F2 
+            z = self.tosz(x - xopt)
+
+            i = (np.arange(self.dim) + 1)
+            if np.size(x.shape) > 1:
+                shape = np.ones(np.size(x.shape))
+                shape[0] = self.dim
+                i = np.broadcast_to(i.reshape(shape.astype(int)), x.shape)
+            f = np.sum(10 ** (6 * (i-1) / (self.dim-1)) * z ** 2, axis=0) + self.fopt
+
+            return f
+
+        #elif 
+
+    def grid(self):
+        x1 = np.linspace(self.lb[0], self.ub[0], 200)
+        x2 = np.linspace(self.lb[1], self.ub[1], 200)
+        x1_mesh, x2_mesh = np.meshgrid(x1, x2)
+        inputs = np.array([x1_mesh, x2_mesh])
+        f = self.evaluate(inputs)
+        return x1_mesh, x2_mesh, f
+
+    def compute_a(self, alpha, grid):
+
+        self.a = np.zeros((self.constr_number, self.dim))
+
+        x1 = grid[0]
+        x2 = grid[1]
+        f = grid[2]
+        f_shape = list(f.shape)
+        f_shape.insert(0, self.dim)
+        grad_f = np.zeros(tuple(f_shape))
+
+        # TODO: check gradient computation
+        for i in range(self.dim):
+            grad_f[i,:] = np.gradient(f, axis=(self.dim-i-1))
+
+        #TODO: find the location of the y_star in grad_f
+        x1_idx = (np.abs(x1[0,:] - self.ystar[0])).argmin()
+        x2_idx = (np.abs(x2[:,0] - self.ystar[1])).argmin()
+        grad_f_ystar = grad_f[:,x2_idx,x1_idx]
+        self.a[0,:] = - alpha * grad_f_ystar / np.linalg.norm(grad_f_ystar)
+        self.a[1:,:] = np.random.normal(self.a[0,:], 0.8, size=(self.constr_number-1,self.dim))
+
+    def initialize_constraint_parameters(self):
+        alpha = np.ones(self.constr_number)
+        b = np.zeros(self.constr_number)
+        self.compute_a(alpha[0], self.grid())
+
+    def compute_g(self, x):
+        #g = alpha * (a @ (x-self.ystar).T) + b
+        g = (self.a @ (x-self.ystar).T)
+        g[1] = g[1] + 2.
+        return -g
--- pre_postprocessing/figure.py
+++ pre_postprocessing/figure.py
@@ -1,29 +1,40 @@
-import numpy as np
-import matplotlib.pyplot as plt
 
+import matplotlib.pyplot as plt
+import numpy as np
 
 class Figure:
-    def __init__(self):
+    def __init__(self, dim):
         self.ax = None
         self.fig = None
+        self.dim = dim
     
     def contour(self, test_function):
-        lb = 2 * test_function.lb
-        ub = 2 * test_function.ub
+        x1_mesh, x2_mesh, f = test_function.grid()
+        self.fig, self.ax=plt.subplots(1,1)    
+        cp = self.ax.contour(x1_mesh, x2_mesh, np.log(f), cmap='rainbow')        
+        self.fig.colorbar(cp)
 
-        xlist = np.linspace(lb, ub, 100)
-        ylist = np.linspace(lb, ub, 100)
-        X, Y = np.meshgrid(xlist, ylist)
-        inputs = np.array([X, Y])
-        Z = test_function.evaluate(inputs)
-        self.fig, self.ax=plt.subplots(1,1)
+        x = np.zeros((f.size, self.dim))
+        x[:, 0] = np.reshape(x1_mesh, f.size)
+        x[:, 1] = np.reshape(x2_mesh, f.size)
         
-        cp = self.ax.contourf(X, Y, Z, cmap="rainbow")
-        self.ax.contour(X, Y, Z, colors="black")
-        self.fig.colorbar(cp) # Add a colorbar to a plot
+        alpha = 0.9
+        g = test_function.compute_g(x)
+        for i in range(test_function.constr_number):
+            g_reshaped = np.reshape(g[i,:], f.shape) < 0         
+            self.ax.contourf(x1_mesh, x2_mesh, g_reshaped, 
+                            cmap='Greys',
+                            alpha=alpha)
+            self.ax.contour(x1_mesh, x2_mesh, g_reshaped, 
+                            linewidths=1.0,
+                            levels=0,
+                            colors='black')        
+            alpha = alpha * 0.7
+
         self.ax.set_title('Coco Function')
     
-    def solution_point(self, solution):
-
-        self.ax.plot(solution[0], solution[1], marker="o", markersize=10, markeredgecolor="red", markerfacecolor="white")
+    def solution_point(self, init, solution, ystar):
+        self.ax.plot(solution[0], solution[1], marker="o", markersize=8, markeredgecolor="black", markerfacecolor="red")
+        self.ax.plot(init[0], init[1], marker="o", markersize=8, markeredgecolor="black", markerfacecolor="blue")
+        self.ax.plot(ystar[0], ystar[1], marker="*", markersize=7, markeredgecolor="black", markerfacecolor="yellow")
         plt.show()
